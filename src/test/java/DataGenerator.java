bef22690903350fc1db1565bfa241274566f8fff
Zebrusha <zebrushaa@mail.ru>
2024-05-21 18:37:15 +0300

    [200~Move the data generator into a separate class

--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DataGenerator {
+    
    public static String generateDate(int addDays , String pattern){
        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
    }
